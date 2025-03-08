6b3a3448dcb86d6a988d1f6febb2900612942fbd
tjtharrison <gh@tjth.co>
2025-03-08 17:33:38 +0000

    feat: Initial commit

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Update instance
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Install docker
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+# Add the repository to Apt sources:
+echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker
+sudo apt-get install docker-ce docker-ce-cli 
+
+# Install other packages
+apt-get install qemu-guest-agent
+
+# Launch docker containers
+sudo docker compose up -d
